42c8b40cdcc54845ea00175a3a040dce44b3a003
Khoa Nguyen (TAU) <khoa.nguyen@tuni.fi>
2019-07-12 17:07:35 +0300

    can train on new dataset

--- scripts/transfer_learning.py
+++ scripts/transfer_learning.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 from pathlib import Path
 import argparse
 import math
+from tqdm import tqdm
 
 from utils.helpers import to_cuda, imshow, imwrite, plot_losses
 from utils.data_handler import InterpolationDataset
@@ -19,10 +20,11 @@ parser.add_argument('--load_model', type=str, default='weights/sepconv_weights')
 parser.add_argument('--save_weights', type=str, default='weights/transfer_learning_weights')
 parser.add_argument('--kernel', type=int, default=51)
 parser.add_argument('--epochs', type=int, default=1)
-parser.add_argument('--batch_size', type=int, default=1)
+parser.add_argument('--batch_size', type=int, default=5)
 
 args = parser.parse_args()
 
+# make the paths from args parser
 project_path = Path(__file__).parent.parent
 train_dataset_path = Path(project_path/args.train_dir)
 val_dataset_path = Path(project_path/args.val_dir)
@@ -37,7 +39,7 @@ epochs = args.epochs
 batch_size = args.batch_size
 
 
-def train():
+def train_and_evaluate():
     # load the model and the weights (for each part)
     model = to_cuda(SepConvNet(kernel))
     model.features.load_state_dict(torch.load(features_weight_path))
@@ -49,83 +51,83 @@ def train():
     val_dataset = InterpolationDataset(val_dataset_path)
     val_loader = DataLoader(val_dataset, batch_size=batch_size)
 
-    # test the model. TODO: move this to the test.py file
-    # first_frame, gt_frame, sec_frame, gt_name = next(iter(train_loader))
-    # print(first_frame.shape, gt_frame.shape, sec_frame.shape)
-    # # imshow(first_frame[0], 'first_frame')
-    # # imshow(sec_frame[0], 'sec_frame')
-    # frame_out = model(first_frame, sec_frame)
-    # print(f'Output shape: {frame_out.shape}')
-    # psnr = -10 * math.log10(torch.mean((gt_frame - frame_out) * (gt_frame - frame_out)).item())
-    # print(psnr)
-    # imshow(frame_out[0], 'frame_out')
-
     # freeze the features part of the model
     for param in model.features.parameters():
         param.requires_grad = False
 
     optimizer = optim.Adam(params=model.subnet_kernel.parameters(), lr=0.001)  # fine-tune the subnet_kernels part
     criterion = nn.MSELoss()
-    steps = 0
+    val_after = 5  # after training this number of batches, do evaluation
     running_loss = 0
-    print_every = 5
     train_losses, val_losses, average_psnr = [], [], []
 
     # training
     for epoch in range(epochs):
-        for first_frame, gt_frame, sec_frame, gt_names in train_loader:
-            steps += 1
-            optimizer.zero_grad()
-            frame_out = model(first_frame, sec_frame)
-            loss = criterion(frame_out, gt_frame)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
+        steps = 0
+        # prepare the output dir for each epoch
+        out_epoch_dir = out_dir / ('epoch' + str(epoch).zfill(3))
+        if not out_epoch_dir.exists():
+            out_epoch_dir.mkdir()
 
-            # evaluation
-            if steps % print_every == 0:
-                val_loss = 0
-                avg_psnr = 0
-                model.eval()
-                with torch.no_grad():
-                    for val_first_frame, val_gt_frame, val_sec_frame, val_gt_names in val_loader:
-                        val_frame_out = model(val_first_frame, val_sec_frame)
-                        batch_loss = criterion(val_frame_out, val_gt_frame)
-                        val_loss += batch_loss.item()
+        with tqdm(total=len(train_loader)) as t:  # use tqdm for progress bar
+            for first_frame, gt_frame, sec_frame, gt_names in train_loader:
+                steps += 1
+                optimizer.zero_grad()
+                frame_out = model(first_frame, sec_frame)
+                loss = criterion(frame_out, gt_frame)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
 
-                        # calculate accuracy (peak signal to noise ratio)
-                        psnr = -10 * math.log10(torch.mean((val_gt_frame - val_frame_out)
-                                                           * (val_gt_frame - val_frame_out)).item())
-                        avg_psnr += psnr
-                        # write the files into output folder
-                        for index, name in enumerate(val_gt_names):
-                            output_path = (out_dir/name).as_posix()
-                            imwrite(val_frame_out[index], output_path)
+                # evaluation
+                if steps % val_after == 0:
+                    print('Evaluating.. ', end=' ')
+                    val_loss = 0
+                    avg_psnr = 0
+                    model.eval()
+                    with torch.no_grad():
+                        for val_first_frame, val_gt_frame, val_sec_frame, val_gt_names in val_loader:
+                            val_frame_out = model(val_first_frame, val_sec_frame)
+                            batch_loss = criterion(val_frame_out, val_gt_frame)
+                            val_loss += batch_loss.item()
 
-                print(f"Step {steps}.. "
-                      f"Epoch {epoch + 1}/{epochs}.. "
-                      f"Train loss: {running_loss / print_every:.3f}.. "
-                      f"Val loss: {val_loss / len(val_loader):.3f}.. "
-                      f"Average PSNR: {avg_psnr / len(val_loader):.3f}")
+                            # calculate accuracy (peak signal to noise ratio)
+                            psnr = -10 * math.log10(torch.mean((val_gt_frame - val_frame_out)
+                                                               * (val_gt_frame - val_frame_out)).item())
+                            avg_psnr += psnr
+                            # write the files into output folder
+                            for index, name in enumerate(val_gt_names):
+                                output_path = (out_epoch_dir / name).as_posix()
+                                imwrite(val_frame_out[index], output_path)
 
-                running_loss = 0
-                model.train()
+                    print(f" Epoch {epoch + 1}/{epochs}.. "
+                          f"Step {steps}.. "
+                          f"Train loss: {running_loss / val_after:.3f}.. "
+                          f"Val loss: {val_loss / len(val_loader):.3f}.. "
+                          f"Average PSNR: {avg_psnr / len(val_loader):.3f}")
 
-                train_losses.append(running_loss / len(train_loader))
-                val_losses.append(val_loss / len(val_loader))
-                average_psnr.append(avg_psnr / len(val_loader))
+                    train_losses.append(running_loss / val_after)
+                    val_losses.append(val_loss / len(val_loader))
+                    average_psnr.append(avg_psnr / len(val_loader))
 
-        # save the model state dict
+                    running_loss = 0
+                    model.train()
+
+                t.update()
+
+        # save the model state dict after each epoch
         state_dict_path = output_weights_path / ('epoch' + str(epoch).zfill(3) + '.pytorch')
         print(f'Saving the model to {state_dict_path}...')
         torch.save({'epoch': epoch,
                     'model_state_dict': model.state_dict(),
                     'kernel_size': kernel,
-                    'train_loss': running_loss / print_every}, state_dict_path)
+                    'train_losses': train_losses,
+                    'val_losses': val_losses,
+                    'avg_psnr': average_psnr}, state_dict_path)
 
         return train_losses, val_losses, average_psnr
 
 
 if __name__ == '__main__':
-    train_losses, val_losses, average_psnr = train()
+    train_losses, val_losses, average_psnr = train_and_evaluate()
     plot_losses(train_losses, val_losses)
