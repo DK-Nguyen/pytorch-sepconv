dca4ae59a6bfedf79173f68199b0741cb72d14fc
Khoa Nguyen (TAU) <khoa.nguyen@tuni.fi>
2019-07-18 18:06:04 +0300

    can deploy and finding the psnr of folders with 1 run

--- scripts/test.py
+++
@@ -1,6 +0,0 @@
-import argparse
-from pathlib import Path
-
-parser = argparse.ArgumentParser(description='testing SepConv Network')
-
-parser.add_argument('--test_dir', type=str, default='data/dslf/test')
